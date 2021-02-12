88fc2a180a2f0d2924e419842b94f29215edcf34
pavanasrivinaya <paripalepu@gmail.com>
2021-02-12 17:56:51 +0530

    cors

--- main/settings.py
+++ main/settings.py
@@ -38,19 +38,26 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
     'users',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', #DJANGO WHITENOISE CONFIGURATION
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+   "https://django-okta.herokuapp.com/",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000"
 ]
 
 ROOT_URLCONF = 'main.urls'
